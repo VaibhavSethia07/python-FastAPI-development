0798bc0949c13c81ce8b75b9184c247737cea892
VaibhavSethia07 <E19CSE321@bennett.edu.in>
2022-07-07 16:02:48 +0530

    Add delete and update methods

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Response, status,HTTPException
+from fastapi import FastAPI, Response, status, HTTPException
 from typing import Optional
 from random import randrange
 from pydantic import BaseModel
@@ -11,7 +11,6 @@ class Post(BaseModel):
     content: str
     published: bool = True
     rating: Optional[int] = None
-    id: int
 
 
 my_posts = [
@@ -19,10 +18,17 @@ my_posts = [
     {"title": "Title of post 2", "content": "Content of post 2", "id": 2},
 ]
 
+
 def find_post(id):
     for post in my_posts:
-        if post['id']==id:
-            return post;
+        if post["id"] == id:
+            return post
+
+
+def find_post_index(id):
+    for idx, post in enumerate(my_posts):
+        if post["id"] == id:
+            return idx
 
 
 @app.get("/")
@@ -35,6 +41,7 @@ def get_posts():
     return {"data": my_posts}
 
 
+# Default status code is provided in the decorator function
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_post(post: Post):
     post_dict = post.dict()
@@ -43,14 +50,52 @@ def create_post(post: Post):
 
     return {"data": post_dict}
 
+
+# Any API with '/posts/some-string' route should be above this route otherwise the path will match with this route and string will be considered as ID
 @app.get("/posts/{id}")
-def get_post(id:int, response:Response):
-    post=find_post(id);
+def get_post(id: int):
+    post = find_post(id)
 
     if not post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Post with id {id} not found")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Post with id: {id} not found",
+        )
 
         # response.status_code=status.HTTP_404_NOT_FOUND;
         # return {"message":  f"Post with id {id} not found"};
+
+    return {"data": post}
+
+
+# When using `HTTP_204_NO_CONTENT` nothing can be returned in response
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    index = find_post_index(id)
+
+    if index == None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Post with id: {id} not found",
+        )
+
+    my_posts.pop(index)
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+    index = find_post_index(id)
+
+    if index == None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Post with id: {id} not found",
+        )
+
+    post_dict = post.dict()
+    post_dict["id"] = id
+    my_posts[index] = post_dict
 
     return {"data": post}
